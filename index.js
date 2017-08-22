47fbec490a3d11e5c031b037d1de53cb7a088cdf
samelie <sam@stinkdigital.com>
2017-08-21 17:51:00 -0700

    make public

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const Q = require('bluebird')
-const fs = require('fs')
-const path = require('path')
-const Dash = require('dash-mp4box-sidx')
-const Downloader = require('youtube-manifest-downloader')
-const GOOGLE = require('google-cloudstorage-commands')
-
+const Q = require("bluebird")
+const fs = require("fs")
+const path = require("path")
+const Dash = require("dash-mp4box-sidx")
+const Downloader = require("youtube-manifest-downloader")
+const GOOGLE = require("google-cloudstorage-commands")
 
 const uploadFile = (p, BUCKET) => {
   return GOOGLE.upload(p, BUCKET, true)
-    .then(() => {})
+    .then(() => GOOGLE.publicRead(BUCKET))
     .catch(err => {
-      console.log(err);
+      console.log(err)
     })
 }
 
@@ -18,20 +17,20 @@ module.exports = (IDS, BUCKET, options = {}) => {
   const save = options.save || __dirname
   try {
     fs.mkdirSync(save)
-  } catch (e) {
-
-  }
-  return Downloader(IDS, BUCKET, { save: save })
-    .then(r => {
-      return Dash(r, { save: save })
-        .then(sidxs => {
-          return Q.map(sidxs, d => {
-            const { name } = path.parse(d.path)
-            fs.writeFileSync(`${name}.json`, JSON.stringify(d))
-            return uploadFile(d.path, BUCKET)
-              .then(() => uploadFile(`${name}.json`, BUCKET))
-          },{concurrency:1})
-        })
+  } catch (e) {}
+  return Downloader(IDS, BUCKET, { save: save }).then(r => {
+    return Dash(r, { save: save }).then(sidxs => {
+      return Q.map(
+        sidxs,
+        d => {
+          const { name } = path.parse(d.path)
+          fs.writeFileSync(`${name}.json`, JSON.stringify(d))
+          return uploadFile(d.path, BUCKET).then(() =>
+            uploadFile(`${name}.json`, BUCKET)
+          )
+        },
+        { concurrency: 1 }
+      )
     })
-
+  })
 }
